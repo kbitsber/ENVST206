55b80589fbce7724bdc2bd76e5578b3e5b7a7b15
Kristen Bitsberger <kristenbitsberger@MacBook-Pro.local>
2020-09-03 20:09:47 -0400

    description of changes

--- Activity 1/activity1_script.r
+++ Activity 1/activity1_script.r
@@ -1,2 +1,24 @@
 #activity 1
 print ("hello R world")
+#comment 
+2+2
+5-2
+6*6
+4/2
+#name my number
+a <- 3
+5*a
+#vector 
+b <- c(3,5,10)
+#divide all numbers by 2
+b/2
+
+#starting question 3 here 
+#equation for question 3
+5+5-(2*2)
+#vector for question 3
+k <- c(500,60,75,150)
+k/5
+#another vector for question 3
+t <- c(1,2,3,4)
+t*3
